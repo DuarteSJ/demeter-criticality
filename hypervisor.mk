1c2eba391b67b8a14be0f3b495331b727a4d9b23
DuarteSJ <ddduarte@sapo.pt>
2026-08-07 18:50:09 +0100

    Import Demeter SOSP'25 artifact (pristine, GPL-2.0)
    Unmodified artifact from Hu et al., Demeter: A Scalable and Elastic Tiered
    Memory Solution for Virtualized Cloud via Guest Delegation (SOSP '25).
    Source: Zenodo https://doi.org/10.1145/3731569.3764801
    Base commit for the thesis fork; all later commits are our changes.

---
+++ hypervisor.mk
@@ -0,0 +1,31 @@
+include environment.mk
+.PHONY: all ch clean build install
+
+all: ch
+
+CH_BASE_TARBALL = cloud-hypervisor-base.tar.gz
+$(CH_BASE_TARBALL):
+	curl -LC- -o $@ \
+		https://github.com/cloud-hypervisor/cloud-hypervisor/archive/refs/tags/v36.0.tar.gz
+
+CH_SOURCE_DIR := hypervisor
+$(CH_SOURCE_DIR):
+	mkdir -p $@
+
+ch: $(CH_SOURCE_DIR)/.stamp
+$(CH_SOURCE_DIR)/.stamp: patch/hypervisor.patch $(CH_BASE_TARBALL) $(CH_SOURCE_DIR)
+	tar -axf $(CH_BASE_TARBALL) --strip-components=1 -C $(CH_SOURCE_DIR)
+	@echo "Applying hypervisor patch..."
+	patch -d $(CH_SOURCE_DIR) -p1 < $<
+	touch $@
+
+build: ch
+	cd $(CH_SOURCE_DIR); cargo build --release --target x86_64-unknown-linux-musl
+
+install: build
+	mkdir -p bin
+	cp -vt bin \
+		$(CH_SOURCE_DIR)/target/x86_64-unknown-linux-musl/release/cloud-hypervisor
+
+clean:
+	rm -rf $(CH_SOURCE_DIR) $(CH_BASE_TARBALL)
---
+++ kernel.mk
@@ -0,0 +1,85 @@
+include environment.mk
+KERNEL_TARGETS := olddefconfig bzImage modules compile_commands.json scripts_gdb
+DEMETER_KERNELS := demeter demeterhost
+SOTA_KERNELS := memtis nomad tpp tpphost
+GUEST_KERNELS := demeter memtis nomad tpp
+HOST_KERNELS := demeterhost tpphost
+JOBS ?= $(shell nproc)
+
+.PHONY: all clean demeter sota \
+	build $(addprefix build-,$(DEMETER_KERNELS) $(SOTA_KERNELS)) \
+	install-guest $(addprefix install-guest-,$(GUEST_KERNELS)) \
+	install-host $(addprefix install-host-,$(HOST_KERNELS))
+all: demeter sota
+
+# Alternative mirrors:
+# https://github.com/torvalds/linux/archive/refs/tags/v6.10.tar.gz
+# https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.10.tar.gz
+# https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.162.tar.gz
+DEMETER_BASE_TARBALL = demeter-base.tar.gz
+$(DEMETER_BASE_TARBALL):
+	curl -LC- -o $@ \
+		https://github.com/gregkh/linux/archive/refs/tags/v6.10.tar.gz
+
+DEMETER_SOURCE_DIR := kernel/demeter
+demeter: $(DEMETER_SOURCE_DIR)/.stamp
+$(DEMETER_SOURCE_DIR)/.stamp: patch/demeter.patch $(DEMETER_BASE_TARBALL) $(DEMETER_SOURCE_DIR)
+	tar -axf $(DEMETER_BASE_TARBALL) --strip-components=1 -C $(DEMETER_SOURCE_DIR)
+	@echo "Applying Demeter patch..."
+	patch -d $(DEMETER_SOURCE_DIR) -p1 < $<
+	touch $@
+
+SOTA_BASE_TARBALL = sota-base.tar.gz
+$(SOTA_BASE_TARBALL):
+	curl -LC- -o $@ \
+		https://github.com/gregkh/linux/archive/refs/tags/v5.15.162.tar.gz
+
+SOTA_SOURCE_DIR := kernel/sota
+sota: $(SOTA_SOURCE_DIR)/.stamp
+$(SOTA_SOURCE_DIR)/.stamp: patch/sota.patch $(SOTA_BASE_TARBALL) $(SOTA_SOURCE_DIR)
+	tar -axf $(SOTA_BASE_TARBALL) --strip-components=1 -C $(SOTA_SOURCE_DIR)
+	@echo "Applying SOTA patch..."
+	patch -d $(SOTA_SOURCE_DIR) -p1 < $<
+	touch $@
+
+$(DEMETER_SOURCE_DIR) $(SOTA_SOURCE_DIR):
+	mkdir -p $@
+
+# $(call make-kernel,<config>,<srctree>,<objtree>[,<targets>])
+make-kernel = $(shell set -ex; \
+						mkdir -p $(3); \
+						[ -f $(3)/.config ] || cp $(1) $(3)/.config; \
+						echo make -j $(JOBS) -C $(2) O=$(3) $(4);)
+build: $(addprefix build-,$(DEMETER_KERNELS) $(SOTA_KERNELS))
+$(addprefix build-,$(DEMETER_KERNELS)): build-%: config/%.config demeter
+	ccache --zero-stats
+	$(call make-kernel,$<,$(@:build-%=$(CURDIR)/kernel/%),$(@:build-%=$(CURDIR)/build/%),$(KERNEL_TARGETS))
+	ccache --show-stats
+
+$(addprefix build-,$(SOTA_KERNELS)): build-%: config/%.config sota
+	ccache --zero-stats
+	$(call make-kernel,$<,$(@:build-%=$(CURDIR)/kernel/%),$(@:build-%=$(CURDIR)/build/%),$(KERNEL_TARGETS))
+	ccache --show-stats
+
+install-guest: $(addprefix install-guest-,$(GUEST_KERNELS))
+$(addprefix install-guest-,$(GUEST_KERNELS)): install-guest-%: build-%
+	mkdir -p $(<:build-%=bin/%)
+	cp -vt $(<:build-%=bin/%) \
+		$(<:build-%=build/%)/**/compressed/vmlinux.bin \
+		$(<:build-%=build/%)/**/*.ko \
+		$(<:build-%=build/%)/**/vmlinux
+
+install-host: $(addprefix install-host-,$(HOST_KERNELS))
+
+install-host-demeterhost: script/linux-6.10.0-demeterhost.conf build-demeterhost
+	sudo make -C build/demeterhost modules_install
+	cd build/demeterhost; sudo installkernel 6.10.0-demeterhost arch/x86_64/boot/bzImage System.map /boot/
+	sudo cp -vt /boot/loader/entries $<
+
+install-host-tpphost: script/linux-5.15.162-tpphost.conf build-tpphost
+	sudo make -C build/tpphost modules_install
+	cd build/tpphost; sudo installkernel 5.15.162-tpphost arch/x86_64/boot/bzImage System.map /boot/
+	sudo cp -vt /boot/loader/entries $<
+
+clean:
+	rm -rf build $(DEMETER_BASE_TARBALL) $(DEMETER_SOURCE_DIR) $(SOTA_BASE_TARBALL) $(SOTA_SOURCE_DIR)
